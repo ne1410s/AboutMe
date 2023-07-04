88277ed203c44a011ea1344acb6a6286ddf1be13
ne1410s <pgj646@gmail.com>
2023-07-04 21:58:35 +0100

    fix test

--- source/AboutMe.Web/src/app/app.component.spec.ts
+++ source/AboutMe.Web/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent],
+    imports: [RouterModule]
   }));
 
   it('should create the app', () => {
