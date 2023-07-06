b54b03cfe28ba232b28526b875c89e4a490eae0a
ne1410s <pgj646@gmail.com>
2023-07-06 22:12:10 +0100

    fix unit test

--- source/AboutMe.Web/src/app/components/root/root.component.spec.ts
+++ source/AboutMe.Web/src/app/components/root/root.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RootComponent } from './root.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 describe('RootComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [RootComponent],
+    declarations: [RootComponent, HeaderComponent, FooterComponent],
     imports: [RouterModule]
   }));
 
