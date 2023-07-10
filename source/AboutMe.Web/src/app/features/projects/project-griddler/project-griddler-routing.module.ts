37adf36bb75f0859b8e90c73cd0596320bb64bd3
paul.jones <paul.jones@animalfriends.co.uk>
2023-07-10 16:45:03 +0100

    add new project page

---
+++ source/AboutMe.Web/src/app/features/projects/project-griddler/project-griddler-routing.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ProjectGriddlerComponent } from './components/project-griddler.component';
+
+const ROUTES = [
+  {
+    path: '',
+    component: ProjectGriddlerComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+  ],
+  exports: [RouterModule],
+})
+export class ProjectGriddlerRoutingModule {}
