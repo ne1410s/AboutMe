b772481e033f1d644c82eca314356747671f0c1f
ne1410s <pgj646@gmail.com>
2023-07-17 21:50:44 +0100

    popup and menu examples

--- source/AboutMe.Web/src/app/features/projects/project-custom-elements/components/project-custom-elements.component.ts
+++ source/AboutMe.Web/src/app/features/projects/project-custom-elements/components/project-custom-elements.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Popup } from '@ne1410s/popup';
 
 @Component({
   selector: 'app-project-custom-elements',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-custom-elements.component.scss'],
 })
 export class ProjectCustomElementsComponent {
+
+  @ViewChild('popup') popup!: ElementRef<Popup>;
+
+  onItemSelect(event: any) {
+    console.log(event.detail.title);
+  }
+
+  onButtonClick() {
+    this.popup.nativeElement.open();
+  }
 }
