bd6e5546855a6dd62ac8a59b1d3f6e6ade99233d
ne1410s <pgj646@gmail.com>
2025-01-16 14:51:14 +0000

    bump to ng v18

--- source/AboutMe.Web/src/app/features/shared/shared.module.ts
+++ source/AboutMe.Web/src/app/features/shared/shared.module.ts
@@ -1,12 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { ImageViewerComponent } from './components/image-viewer.component';
 import { CommonModule } from '@angular/common';
 
-@NgModule({
-  declarations: [ImageViewerComponent],
-  imports: [CommonModule, HttpClientModule],
-  exports: [ImageViewerComponent],
-})
+@NgModule({ declarations: [ImageViewerComponent],
+    exports: [ImageViewerComponent], imports: [CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule {}
